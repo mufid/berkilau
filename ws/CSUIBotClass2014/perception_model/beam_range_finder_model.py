383824f2be2606981f56071ac2da9e948d02d3f2
Evan Ariansyah <evan.ariansyah@gmail.com>
2014-06-21 09:33:58 +0700

    beam_range_finder_model: pshort, pfail, prand

--- ws/CSUIBotClass2014/perception_model/beam_range_finder_model.py
+++ ws/CSUIBotClass2014/perception_model/beam_range_finder_model.py
@@ -10,7 +10,7 @@ def phit(ztk, x_t, m, sensor_direction):
   ztk_star = rc.range((x_t['x'], x_t['y']),
     result_raycast)
 
-  if (ztk >= 0 && ztk < zmax):
+  if (ztk >= 0 and ztk < zmax):
     funct = (1/math.sqrt(2*math.pi*sigmasq))*math.exp(
       -0.5*((ztk - ztk_star)**2)/sigmasq)
     
@@ -22,6 +22,28 @@ def phit(ztk, x_t, m, sensor_direction):
   else:
     return 0 
 
+def pshort(ztk, x_t, m, sensor_direction):
+  lambdashort = 0.5 #intrinsic parameter
+  result_raycast = rc.ray_casting(x_t, m, sensor_direction)
+  ztk_star = rc.range((x_t['x'], x_t['y']),
+    result_raycast)
+  if(ztk >= 0 and ztk_star >= ztk):
+    nu = 1/(1-math.exp(-lambdashort*ztk_star))
+    expval = nu*lambdashort*math.exp(-lambdashort*ztk)
+    return expval
+  else:
+    return 0
+
+def pfail(ztk):
+  zmax = 3.0
+  return ztk == zmax #max range = 3
+
+def prand(ztk):
+  zmax = 3.0
+  if(0 <= ztk and ztk < zmax):
+    return 1/zmax
+  else:
+    return 0
 
 def beam_range_finder_model(z_t, x_t, m):
   q = 1
@@ -37,7 +59,17 @@ def beam_range_finder_model(z_t, x_t, m):
     z_tk = rc.range((x_t['x'], x_t['y']),
       result_raycast)
     p_hit = p_hit(z_tk, x_t, m, sensor_direction)
-    p = 0.7 # dummy
+    
+    #intrinsic parameter, totalnya harus = 1
+    zhit = 0.25
+    zshort = 0.25
+    zfail = 0.25
+    zrand = 0.25
+
+    p = (zhit * phit(z_tk, x_t, m, sensor_direction))*
+        (zshort * pshort(z_tk, x_t, m, sensor_direction))*
+        (zfail * pfail(z_tk))*
+        (zrand * prand(z_tk))
     q = q * p
 
   return q
